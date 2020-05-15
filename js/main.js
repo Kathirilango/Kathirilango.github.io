18cdd470492f0d56be296fa76f945232be215b3d
Kathir Ilango <kathirilango@MacBook-Pro-5.local>
2020-05-15 15:14:03 -0700

    Personal Website Version 2.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+//Close navbar on click
+$('.navbar-collapse a').click(function(){
+	$(".navbar-collapse").collapse('hide');
+});
+
 //Typing Text Animation
 var text = "I'm Kathir.";
 var textArr = text.split("");
@@ -22,14 +27,35 @@ setTimeout('frameLooper()', 2000);
 const workItems = document.querySelectorAll('.card');
 workItems.forEach(workItem => {
 	workItem.addEventListener('mouseover', () => {
-		workItem.childNodes[1].classList.add('img-darken');
+		workItem.childNodes[1].childNodes[0].classList.add('img-darken');
+		
 	});
 	workItem.addEventListener('mouseout', () => {
-		workItem.childNodes[1].classList.remove('img-darken');
+		workItem.childNodes[1].childNodes[0].classList.remove('img-darken');
 	});
 })
 
 //Email Popover
 $(document).ready(function(){
 	$('[data-toggle="popover"]').popover();
+});
+
+//Keyboard animation
+var playPause = anime({
+	targets: ['div.key',  'div.space', 'div.entershift', 'div.caps', 'div.tab', 'div.shift', 'div.backspace'],
+	translateY: [
+	  { value: 150, duration: 10000 },
+	  { value: 0, duration: 10000 }
+	],
+	rotate:{
+	  value: '2turn',
+	  easing: 'easeInOutQuad'
+    },
+    duration: 20000,
+	autoplay:true,
+    loop:true,
+    delay: function(el, i, l){ return i * 100},
+});
+jQuery(window).scroll(function() {
+    playPause.play;
 });
